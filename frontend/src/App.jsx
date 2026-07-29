33a3b7ae00b9c2ab0c3df80c6d119e27afbda0c2
kenzaboualili <kenza.bll78@gmail.com>
2026-07-29 23:13:52 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1864,7 +1864,6 @@ function App() {
                 </div>
               </div>
             )}   
-        )}
         
        {/* MODULE DEVIS & CRÉATION DE DEVIS AVEC DESIGN AVANCÉ */}
         {vueActuelle === 'devis' && (
